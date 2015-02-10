b9320f0577981f35a12c213bf930a43820c0b219
Josetosanz <josetosanz@yahoo.co.nz>
2015-02-11 10:44:17 +1300

    Updated on 10th Feb 2015

---
+++ blog.php
@@ -0,0 +1,287 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Bronwyn Abbott | Gallery</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Perfect masonry isotope portfolio template. Masonry portfolio examples. Build your portfolio with berger template." />
+    <meta name="keywords" content="masonry portfolio, portfolio design, portfolio template, minimal portfolio, masonry isotope portfolio " />
+    <meta name="author" content="ClaPat Studio">
+    <meta charset="UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+        
+    <link rel="icon" type="image/ico" href="images/favicon.ico" />
+    <link href="style.css" rel="stylesheet" />
+
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>    
+
+</head>
+    
+
+
+<body>
+    <!-- Modal -->
+                        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                          <div class="modal-dialog">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                              </div>
+                              <div class="modal-body">
+                                ...
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+
+
+
+
+    <div class="clapat-mask"><div id="clapatloader"></div></div>
+
+
+
+
+    <!-- Header -->
+    <header class="first-load">
+    
+                            
+        <!-- Header Wrapper -->                    
+        <div id="header-wrapper">
+                   
+            
+            <!-- Menu Button-->
+            <a class="btn_menu clapat-menubtn">
+            <span class="btn_menu_line"></span>
+            <span class="btn_menu_line"></span>
+            <span class="btn_menu_line"></span>
+            <div style="margin-top:-30px; margin-left:-3px;">Menu</div>
+            </a>
+            
+            <!--/Menu Button-->
+            
+            
+            
+            <!-- Secondary Menu-->
+            <div id="secondary-menu" class="hide-secondary">
+                
+                <ul class="menu">
+                </ul>
+                
+            </div>
+            <!--/Secondary Menu-->
+            
+            
+            
+            <!-- Overlay Menu-->
+            <?php include("menu.html"); ?>
+            <!--/Overlay Menu-->
+            
+            
+            
+        </div>
+        <!--/Header Wrapper -->
+        	
+        
+        
+    
+    </header>
+    <!--/Header -->
+
+
+	<!-- Content -->
+    <div id="content"><div id="content-ajax">
+        
+        
+        <!-- Main --> 
+        <div id="main">
+            
+            <!-- Container -->    
+           	<div class="container">
+            	
+                 
+            <!-- Blog -->
+                <div id="blog-post">
+                               
+                
+                
+                    <!-- Post Header -->
+                    <div class="post-header">
+                    
+                        <ul class="meta-categories title-has-line">
+                            <li><a href="#">Web Design</a></li>
+                            <li><a href="#">Branding</a></li>
+                        </ul>
+                        
+                        <h1 class="post-title-no-link">One Story About Nothing</h1>
+                        
+                        <ul class="entry-meta">
+                            <li class="entry-date"><a href="#">13 November</a></li>
+                            <!-- <li class="entry-author">Posted by <a href="#">Admin</a></li>
+                            <li class="entry-comments"><a href="#">No Comments</a></li> -->
+                        </ul>
+                    
+                    </div>
+                    <!--/Post Header -->
+                    
+                    
+                    
+                    <!-- Post Content -->
+                    <div class="post-content">
+                    
+                        <p class="artist">Nullam ornare, sem in malesuada sagittis, quam sapien ornare massa, id pulvinar quam augue vel orci. Praesent leo orci cursus ac malesuada</p>
+                        
+                        <p class="artist">Lorem ipsum dolor sit amet, consec adipiscing elit. Aliquam nisi lorem, pulvinar id, commodo feugiat, vehicula et, mauris. </p>
+                        
+                        <blockquote>Praesent quis mauris ligula. Sed et vestibulum risus. Etiam non sollicitudin turpis, at venenatis sem. Nulla tristique</blockquote>
+                        
+                        <p class="artist">Cum magnis dis parturient montes, nascetur ridiculus mus. Suspendisse sed ligula. Sed volutpat odio non turpis gravida luctus. Praesent elit pede, iaculis facilisis, vehicula mattis, tempus non, arcu.</p>
+                        
+                        <p class="artist">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam imperdiet justo risus, eu convallis lorem pretium ac. Praesent quis mauris ligula. Sed et vestibulum risus. Etiam non sollicitudin turpis, at venenatis sem. Nulla tristique, massa et elementum dapibus, risus arcu tincidunt sapien, quis aliquet ante mi vel libero.</p>
+        
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet.</li>
+                            <li>Consectetuer adipiscing elit.</li>
+                            <li>Nam at tortor quis ipsum tempor aliquet.</li>
+                        </ul>   
+        
+                        <p class="artist">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam imperdiet justo risus, eu convallis lorem pretium ac. Praesent quis mauris ligula. Sed et vestibulum risus. Etiam non sollicitudin turpis, at venenatis sem. Nulla tristique, massa et elementum dapibus, risus arcu tincidunt sapien, quis aliquet ante mi vel libero.</p>
+                        
+                    </div>
+                    <!--/Post Content -->
+                    
+                    
+                    
+                    
+                    
+                    <!-- Blog Navigation -->
+                    <ul class="blog-nav">                
+                        <li class="prev-posts"><a href="#">Prev Post</a></li>
+                        <li class="next-posts"><a href="#">Next Post</a></li>                
+                    </ul>
+                    <!--/Blog Navigation -->
+
+                    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                        White
+                    </button>
+
+
+                    
+
+
+
+
+                    <hr><hr><hr>
+                    <!-- Diques Comment -->
+                    <div id="disqus_thread"></div>
+                        <script type="text/javascript">
+                            /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                            var disqus_shortname = 'bronwynabbott-artist'; // required: replace example with your forum shortname
+
+                            /* * * DON'T EDIT BELOW THIS LINE * * */
+                            (function() {
+                                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                            })();
+                        </script>
+                        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    
+
+                    
+                
+                
+                </div>
+                <!--/Blog -->
+
+            </div>       
+            <!--/Container -->
+            
+        </div>                
+        <!--/Main -->   
+		
+        
+        <!-- Scroll To Top -->
+        <div class="scrolltotop"><div class="top-arrow"></div></div>
+        <!--/Scroll To Top -->
+    
+    
+    </div></div>
+	<!--/Content -->
+
+
+
+
+
+	<!-- Footer -->
+    <?php include("footer.html"); ?>
+    <!--/Footer -->
+    
+
+	<script src="js/jquery.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/scripts.js"></script>	
+    
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-7069518-8']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+
+
+
+    <!-- Disqus Js script -->
+    <script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'bronwynabbott-artist'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+    </script>
+    
+
+
+    <!-- Comment for facebook -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+</body>
+
+</html>
